dff953fe60cc243f1e20823398f0e514e1c72ed4
venelrene <venelrene@gmail.com>
2018-12-11 22:27:02 -0500

    controller spec

---
+++ testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe PeopleController do
+  describe "#create" do
+    context "when person is valid" do
+      it "redirects to #show" do
+        person = stub_valid_person
+        post :create, person: { first_name: "John" }
+        expect(response).to redirect_to(person)
+      end
+    end
+
+    context "when person is invalid" do
+      it "renders new" do
+        person = stub_invalid_person
+        post :create, person: { first_name: "James" }
+        expect(response).to render_template(:new)
+      end
+    end
+
+    def stub_valid_person
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).and_return person
+        allow(person).to receive(:save).and_return(true)
+      end
+    end
+
+    def stub_invalid_person
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).and_return person
+        allow(person).to receive(:save).and_return(false)
+      end
+    end
+  end
+end
